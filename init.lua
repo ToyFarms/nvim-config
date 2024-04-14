a2ee53dc212f82de53e117fd7f97135057e2d71c
ToyFarms <diandrashafarrahman1@gmail.com>
2024-04-14 16:39:50 +0700

    Fix bufferline color, and init setting before plugin

--- init.lua
+++ init.lua
@@ -1,20 +1,9 @@
-local powershell_options = {
-    shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
-    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-    shellquote = "",
-    shellxquote = "",
-}
-
-for option, value in pairs(powershell_options) do
-    vim.opt[option] = value
-end
+require("setting")
 require("init_plugins").init()
 vim.cmd([[colorscheme onedark]])
+vim.cmd([[colorscheme onedark]]) -- idk why, required to fix bufferline color
 
 require("custom_plugin.quickcmd.quickcmd").init()
 
-require("setting")
 require("keybind")
 require("command")
